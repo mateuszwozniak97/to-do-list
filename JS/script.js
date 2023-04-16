3575d201f1b797ae27185e9abcce441c22df1e9f
Mateusz Woźniak <mw54544@gmail.com>
2023-04-16 10:05:11 +0200

    remove tasks

--- JS/script.js
+++ JS/script.js
@@ -1,30 +1,43 @@
 {
     const tasks = [
-        {content:" nagrać lekcje",
-        done: false
-    },
-    {
-       content:"zjeść pierogi",
-       done: true 
-    },
-    ]
-    const render = () => {
-        let htmlString = ""
-        for (const task of tasks) {
-            htmlString += `
-            <li 
-            ${task.done ? "style=\"text-decoration: line-through\"" : ""}> 
-            ${task.content}
-            </li>
-            `
-        }
-        document.querySelector(".js-tasks").innerHTML = htmlString
-    }
+        {
+            content: " nagrać lekcje",
+            done: false
+        },
+        {
+            content: "zjeść pierogi",
+            done: true
+        },
+    ];
     const addNewTask = (newTaskContent) => {
         tasks.push({
             content: newTaskContent,
         })
         render();
+    }
+    const removeTask = (index) => {
+        tasks.splice(index, 1);
+                render();
+    }
+    const render = () => {
+        let htmlString = ""
+        for (const task of tasks) {
+            htmlString += `
+            <li 
+            ${task.done ? "style=\"text-decoration: line-through\"" : ""}
+            >
+            <button class="js-remove">Usuń</button> 
+            ${task.content}
+            </li>
+            `
+        }
+        document.querySelector(".js-tasks").innerHTML = htmlString
+        const removeButtons = document.querySelectorAll(".js-remove");
+        removeButtons.forEach((removeButton, index) => {
+            removeButton.addEventListener("click", () => {
+                removeTask(index);
+            })
+        })
     }
     const onForSubmit = (event) => {
         event.preventDefault();
@@ -37,7 +50,7 @@
     const init = () => {
         render();
         const form = document.querySelector(".js-form");
-        form.addEventListener("submit", onForSubmit); 
+        form.addEventListener("submit", onForSubmit);
     };
 
     init();
